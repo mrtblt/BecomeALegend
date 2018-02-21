3540e8532097afc6ff12bf342883df8e56ded039
etrkk5 <etrkk5@gmail.com>
2018-02-21 15:54:59 +0300

    add project files

--- app/src/main/java/com/mert/becomealegend/MainActivity.java
+++ app/src/main/java/com/mert/becomealegend/MainActivity.java
@@ -43,6 +43,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
         textViewSignUp.setOnClickListener(this);
         buttonLogin.setOnClickListener(this);
+
     }
 
     public void userLogin(){
